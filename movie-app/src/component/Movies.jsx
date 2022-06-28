e63ec22c997812f50038a31326a1de5147fa9b28
Natan Chabbat <chabbatnatan@gmail.com>
2022-06-28 06:46:49 +0300

    css

--- movie-app/src/component/Movies.jsx
+++ movie-app/src/component/Movies.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { getMovies } from "../service/fakeMovieService";
+import dayjs from "dayjs";
+import { v4 as uuidv4 } from 'uuid';
+import "./film.css";
 
-
+console.log(uuidv4());
 export class Movies extends Component {
     state = {
         movies: getMovies(),
@@ -10,14 +13,17 @@ export class Movies extends Component {
         const movies = this.state.movies.filter((m)=> m._id !== movie._id)
         this.setState({ movies: movies})
     }
+ 
 
        render() {
         const counter = this.state.movies.length;
         
         return (
             <>
-            <h1>Listes des films</h1>
-            <h2>il y a {counter} film(s) dans la liste</h2>
+            {/* <h1>{dayjs().isBefore(dayjs())}</h1 > */}
+            {/* <h1 className='top'></h1> */}
+            <h2 className='top'>Il y a {counter} film(s) dans cette liste de film.
+            <br />Listes des films de cinema:</h2>
         <table className="table">    
             <thead>
                 <tr>
@@ -25,6 +31,7 @@ export class Movies extends Component {
                     <th>Title</th>
                     <th>Genre</th>
                     <th>Note</th>
+                    <th>  </th>
                 </tr>
             </thead>
             <tbody> 
